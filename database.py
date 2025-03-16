aa0b7fa5ce02ce39b25fae42e2de106cb9eec418
Kerim_Lex <lexmeins1@gmail.com>
2025-03-16 16:27:50 +0500

    ADDED: delete_dish change_dish

--- database.py
+++ database.py
@@ -25,5 +25,33 @@ async def add_dish(dish: Dish):
     await dish.create()
 
 
+async def delete_dish(name: str):
+    await Dish.find_one(Dish.name == name).delete()
+
+
+async def change_name_dish(old_name: str, name: str):
+    dish = await Dish.find_one(Dish.name == old_name)
+    if not dish:
+        raise ValueError(f"Dish with name '{old_name}' not found")
+
+    await dish.update({"$set": {"name": name}})
+
+
+async def change_price_dish(old_price: float, price: float):
+    dish = await Dish.find_one(Dish.price == old_price)
+    if not dish:
+        raise ValueError(f"Dish with price '{old_price}' not found")
+
+    await dish.update({"$set": {"price": price}})
+
+
+async def change_category_dish(old_category: str, category: str):
+    dish = await Dish.find_one(Dish.category == old_category)
+    if not dish:
+        raise ValueError(f"Dish with category '{old_category}' not found")
+
+    await dish.update({"$set": {"category": category}})
+
+
 if __name__ == "__main__":
     asyncio.run(connect())
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, HTTPException
-from database import get_dish, get_dishes, connect, add_dish
+from database import (get_dish, get_dishes, connect, add_dish, delete_dish,
+                      change_name_dish, change_price_dish, change_category_dish)
 from models import Dish
 import uvicorn
 from contextlib import asynccontextmanager
@@ -30,6 +31,26 @@ async def get_dish_api(category: str):
 async def add_dish_api(name: str, price: float, category: str):
     dish = Dish(name=name, price=price, category=category)
     await add_dish(dish)
+
+
+@app.delete("/delete-dish/{name}")
+async def delete_dish_api(name: str):
+    await delete_dish(name)
+
+
+@app.post("/change-dish/name/{old_name}/{name}")
+async def change_dish_api(old_name: str, name: str):
+    await change_name_dish(old_name=old_name, name=name)
+
+
+@app.post("/change-dish/price/{old_price}/{price}")
+async def change_dish_api(old_price: float, price: float):
+    await change_price_dish(old_price=old_price, price=price)
+
+
+@app.post("/change-dish/category/{old_category}/{category}")
+async def change_dish_api(old_category: str, category: str):
+    await change_category_dish(old_category=old_category, category=category)
 
 
 if __name__ == "__main__":
