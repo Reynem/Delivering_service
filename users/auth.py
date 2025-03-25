36c506b6edf8bf7c1a2e2f80aaae6d219bff41b6
Kerim_Lex <lexmeins1@gmail.com>
2025-03-25 23:34:37 +0500

    ADDED: auth.py: JWT tokens to get user

---
+++ users/auth.py
@@ -0,0 +1,41 @@
+from datetime import datetime, timedelta
+from typing import Optional
+from jose import JWTError, jwt
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from users.encryption import verify_password
+from models import User
+
+
+SECRET_KEY = "asdf"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + (expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid authentication credentials",
+        headers={"WWW-Authenticate": "Bearer"}
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email: str = payload.get("sub")
+        if email is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+
+    user = await User.find_one(User.email == email)
+    if user is None:
+        raise credentials_exception
+    return user
--- users/router.py
+++ users/router.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter, HTTPException, Body
+from fastapi import APIRouter, HTTPException, Body, Depends
 from users.database import login_user, register_user
 from models import User, UserCreate
+from users.auth import create_access_token, get_current_user
 
 router = APIRouter()
 
@@ -12,10 +13,18 @@ async def login_user_api(user_data: dict = Body(...)):
     user = await login_user(email, password)
     if user is None:
         return HTTPException(status_code=400, detail="Invalid password or email")
-    return {"status": 200}
+
+    authentication_token = create_access_token({"sub": user.get('email')})
+    return {"status": 200, "access_token": authentication_token, "token_type": "bearer"}
 
 
 @router.post("/register/")
 async def register_user_api(user: UserCreate):
     user = await register_user(user)
     return user
+
+
+@router.get("/users/me/")
+async def read_users_me(current_user: User = Depends(get_current_user)):
+    return current_user
+
